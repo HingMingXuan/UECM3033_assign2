1839d5d380985d5c32dc9fa95201f390dd9ae3da
HingMingXuan <1205182@1utar.edu.my>
2016-03-11 00:40:15 +0800

    Added task1.py

--- task1.py
+++ task1.py
@@ -1,39 +1,49 @@
 import numpy as np
 LIMIT_OF_ITERATION = 10
 
-def lu(A, b):
-    sol = []
-    n = len(A)
-    for k in range(0,n-1): 
-        for i in range(k+1,n):
-            if A[i,k]!=0.0:
-                lam=A[(i,k)]/A[(k,k)]
-    A[i,k+1:n]=A[i,k+1:n]-lam*A[k,k+1:n]
-    A[i,k]=lam
-    return A
+def LUdecomp(A):
+       n=len(A)
+       for k in range(0, n-1):
+        for i in range(k+1, n):
+            if A[i,k]!= 0.0:
+                lam = A[i,k] / A[k,k]
+                A[i, k+1:n] = A[i, k+1:n] - lam * A[k, k+1:n]
+                A[i, k] = lam
+        return A
+def omegafind(A,D):
+    K = np.linalg.inv(D).dot(D-A)
+    eig = max(np.linalg.eigvals(K));
+    omega = 2*(1-np.sqrt(1-eig**2))/eig**2
+    return omega;
     
-    for k in range(1,n):
-        b[k]=b[k]-np.dot(A[k,0:k],b[0:k])
-        b[n-1]=b[n-1]/A[n-1,n-1]
-        for k in range(n-2,-1,-1):
-            b[k]=(b[k]-np.dot(A[k,k+1:n],b[k+1:n]))/A[k,k]
-            return b
-            
-            return list(sol)
+def lu(A, b):
+      A=LUdecomp(A)
+      n = len(A)
+      for k in range(1,n):
+           b[k] = b[k] - np.dot(A[k,0:k], b[0:k])
+           b[n-1]=b[n-1]/A[n-1, n-1]
+      for k in range(n-2, -1, -1):
+           b[k] = (b[k] - np.dot(A[k,k+1:n], b[k+1:n]))/A[k,k]
+      return b
 
 def sor(A, b):
     sol = []
-    omega = 1.03
+    D = np.zeros_like(A)
+    omega=omegafind(A,D)
     
     x = np.zeros_like(b)
     for itr in range(LIMIT_OF_ITERATION):
-            for j in range(len(b)):
-                sum=np.dot(A[j,:],x)
-                x[j]=x[j]+omega*(b[j]-sum)/A[j,j]
+        for j in range(len(b)):
+            sums = np.dot( A[j,:], x )
+            x[j] = x[j] + omega*(b[j]-sums)/A[j,j]
     return list(sol)
 
 def solve(A, b):
-    condition = True
+    condition = False
+    try:       
+        np.linalg.cholesky(A)
+    except np.linalg.linalg.LinAlgError :
+        condition = True
     if condition:
         print('Solve by lu(A,b)')
         return lu(A,b)
@@ -47,7 +57,9 @@ if __name__ == "__main__":
 
     A = np.array([[2,1,6], [8,3,2], [1,5,1]]).astype(float)
     b = np.array([9, 13, 7]).astype(float)
+    
     sol = np.linalg.solve(A,b)
+    solve(A,b)
     print(sol)
     
     A = np.array([[6566, -5202, -4040, -5224, 1420, 6229],
@@ -58,4 +70,5 @@ if __name__ == "__main__":
          [-6903, 5610, 4306, 5548, -1380, 3539.]]).astype(float)
     b = np.array([ 17603,  -63286,   56563,  -26523.5, 103396.5, -27906]).astype(float)
     sol = np.linalg.solve(A,b)
+    solve(A,b)
     print(sol)
